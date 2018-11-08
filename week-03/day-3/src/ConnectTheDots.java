3f510ee9ab143fb45966e97380bc26e01ea11bf9
Adél Bíró <b.adel93@gmail.com>
2018-11-08 11:32:37 +0100

    connecting dots function is working now

---
+++ week-03/day-3/src/ConnectTheDots.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ConnectTheDots {
+    public static void connectTheDots(Graphics graphics, int[][] inputArray) {
+        graphics.setColor(new Color(0,128,0));
+        int[] xCoord = new int[inputArray.length];
+        int[] yCoord = new int[inputArray.length];
+        for (int j = 0; j < inputArray.length; j++) {
+            xCoord[j] = inputArray[j][0];
+            yCoord[j] = inputArray[j][1];
+        }
+        graphics.drawPolygon(xCoord, yCoord, inputArray.length);
+    }
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 1 parameter:
+        // An array of {x, y} points and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+        int[][] coordinatesArray = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] randomCoordinates = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+                {120, 100}, {85, 130}, {50, 100}};
+        connectTheDots(graphics, coordinatesArray);
+        connectTheDots(graphics, randomCoordinates);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
