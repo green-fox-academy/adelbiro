984b71556fe5a1f4236aa8f12c2c3672ca721dbc
Adél Bíró <b.adel93@gmail.com>
2018-11-13 12:50:49 +0100

    Animal done

---
+++ week-04/day-2/src/animal/Animal.java
@@ -0,0 +1,22 @@
+package animal;
+
+public class Animal {
+    int hunger = 50;
+    int thirst = 50;
+
+    //public Animal(int hunger, int thirst) {
+      //  this.hunger = hunger;
+      //  this.thirst = thirst;
+   // }
+    public void eat() {
+        hunger--;
+    }
+    public void drink() {
+        thirst--;
+    }
+    public void play() {
+        hunger++;
+        thirst++;
+    }
+
+}
