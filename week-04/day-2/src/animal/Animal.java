cdbf41ae15585c86a0e64c6113fa0cf794e948ca
Adél Bíró <b.adel93@gmail.com>
2018-11-14 16:21:05 +0100

    Animal modified to help working the Farm

--- week-04/day-2/src/animal/Animal.java
+++ week-04/day-2/src/animal/Animal.java
@@ -1,13 +1,16 @@
 package animal;
 
 public class Animal {
-    int hunger = 50;
+    public int hunger = 50;
     int thirst = 50;
 
-    //public Animal(int hunger, int thirst) {
-      //  this.hunger = hunger;
-      //  this.thirst = thirst;
-   // }
+    public Animal() {
+        this(50, 50);
+    }
+    public Animal(int hunger, int thirst) {
+      this.hunger = hunger;
+      this.thirst = thirst;
+    }
     public void eat() {
         hunger--;
     }
