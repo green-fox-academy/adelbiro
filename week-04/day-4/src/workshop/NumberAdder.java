343e82e4d395e36b00b9cea5e7324d7bded9452e
Adél Bíró <b.adel93@gmail.com>
2018-11-15 12:19:47 +0100

    NumberAdder simplified

--- week-04/day-4/src/workshop/NumberAdder.java
+++ week-04/day-4/src/workshop/NumberAdder.java
@@ -2,13 +2,12 @@ package workshop;
 
 public class NumberAdder {
   public static void main(String[] args) {
-    System.out.println(numberAdder(3));
+    System.out.println(numberAdder(10));
   }
   public static int numberAdder(int addTillThis) {
-    int sum = 0;
     if (addTillThis > 0) {
-      sum = addTillThis + (numberAdder(addTillThis-1));
+      return addTillThis + (numberAdder(addTillThis-1));
     }
-    return sum;
+    return 0;
   }
 }
