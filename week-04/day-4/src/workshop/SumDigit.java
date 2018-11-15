41af0e435a63969ec174fca53fdeef9dc922696b
Adél Bíró <b.adel93@gmail.com>
2018-11-15 11:20:28 +0100

    SumDigit added

---
+++ week-04/day-4/src/workshop/SumDigit.java
@@ -0,0 +1,16 @@
+package workshop;
+
+public class SumDigit {
+  public static void main(String[] args) {
+    System.out.println(sumDigits(11111));
+
+  }
+  public static int sumDigits(int number) {
+    int sum = 0;
+    int mod = number%10;
+    if (mod > 0) {
+      sum = sum + mod + sumDigits(number/10);
+    }
+  return sum;
+  }
+}
