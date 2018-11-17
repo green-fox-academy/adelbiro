6e34a5f5ac3c7a2ff56cf4e6fa7c5fb6db6d3f29
Adél Bíró <b.adel93@gmail.com>
2018-11-17 14:41:24 +0100

    style fixed in NumberAdder.java

--- week-04/day-4/src/workshop/NumberAdder.java
+++ week-04/day-4/src/workshop/NumberAdder.java
@@ -4,9 +4,10 @@ public class NumberAdder {
   public static void main(String[] args) {
     System.out.println(numberAdder(10));
   }
+
   public static int numberAdder(int addTillThis) {
     if (addTillThis > 0) {
-      return addTillThis + (numberAdder(addTillThis-1));
+      return addTillThis + (numberAdder(addTillThis - 1));
     }
     return 0;
   }
