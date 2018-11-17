d008f393859454a1a8a69f50d046c682bcf0a614
Adél Bíró <b.adel93@gmail.com>
2018-11-17 15:07:37 +0100

    reformat in google style

--- week-04/day-1/src/CopyFile.java
+++ week-04/day-1/src/CopyFile.java
@@ -7,21 +7,22 @@ import java.util.List;
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
 public class CopyFile {
-    public static void main(String[] args) {
-        copyFiles("list1.txt", "list2.txt");
+  public static void main(String[] args) {
+    copyFiles("list1.txt", "list2.txt");
+  }
+
+  public static void copyFiles(String copyThisFile, String toThisFile) {
+    Path pathToCopyThis = Paths.get(copyThisFile);
+    Path pathToThisFile = Paths.get(toThisFile);
+    boolean success;
+    try {
+      List<String> tempList = Files.readAllLines(pathToCopyThis);
+      Files.readAllLines(pathToThisFile);
+      Files.write(pathToThisFile, tempList);
+      success = true;
+    } catch (Exception e) {
+      success = false;
     }
-    public static void copyFiles(String copyThisFile, String toThisFile) {
-        Path pathToCopyThis = Paths.get(copyThisFile);
-        Path pathToThisFile = Paths.get(toThisFile);
-        boolean success;
-        try {
-            List<String> tempList = Files.readAllLines(pathToCopyThis);
-            Files.readAllLines(pathToThisFile);
-            Files.write(pathToThisFile, tempList);
-            success = true;
-        } catch (Exception e) {
-            success = false;
-        }
-        System.out.println(success);
-    }
+    System.out.println(success);
+  }
 }
--- week-04/day-1/src/CountLines.java
+++ week-04/day-1/src/CountLines.java
@@ -4,19 +4,20 @@ import java.nio.file.Paths;
 
 
 public class CountLines {
-// Write a function that takes a filename as string,
-// then returns the number of lines the file contains.
-// It should return zero if it can't open the file, and
-// should not raise any error.
-    public static void countTheLines(String fileName) {
-        Path filePath = Paths.get(fileName);
-        try {
-            System.out.println(Files.lines(filePath).count());
-        } catch (Exception e){
-            System.out.println("0");
-        }
+  // Write a function that takes a filename as string,
+  // then returns the number of lines the file contains.
+  // It should return zero if it can't open the file, and
+  // should not raise any error.
+  public static void countTheLines(String fileName) {
+    Path filePath = Paths.get(fileName);
+    try {
+      System.out.println(Files.lines(filePath).count());
+    } catch (Exception e) {
+      System.out.println("0");
     }
-    public static void main(String[] args) {
-        countTheLines("src/my-file.txt");
-    }
+  }
+
+  public static void main(String[] args) {
+    countTheLines("src/my-file.txt");
+  }
 }
