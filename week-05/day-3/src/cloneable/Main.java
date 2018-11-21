49535e355e10057602a08a38adf3a874a9dcda23
Adél Bíró <b.adel93@gmail.com>
2018-11-21 12:30:16 +0100

    added Cloneable formatted version

--- week-05/day-3/src/cloneable/Main.java
+++ week-05/day-3/src/cloneable/Main.java
@@ -6,8 +6,6 @@ package cloneable;
 //    Instantiate John, a 20 years old male from BME
 //    Clone him into jonhTheClone
 
-import com.sun.jmx.snmp.SnmpInt;
-
 public class Main {
   public static void main(String[] args) {
     Student john = new Student("John", 20, "male", "BME");
--- week-05/day-3/src/cloneable/Student.java
+++ week-05/day-3/src/cloneable/Student.java
@@ -9,15 +9,18 @@ public class Student extends Person implements Cloneable {
     previousOrganization = "The School of Life";
     skippedDays = 0;
   }
+
   public Student(String name, int age, String gender, String previousOrganization) {
     super(name, age, gender);
     this.previousOrganization = previousOrganization;
     skippedDays = 0;
   }
+
   public void introduce() {
     System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " from "
         + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
   }
+
   public void getGoal() {
     System.out.println("My goal is: Be a junior software developer.");
   }
@@ -26,6 +29,7 @@ public class Student extends Person implements Cloneable {
     Student clone = new Student(this.name, this.age, this.gender, this.previousOrganization);
     return clone;
   }
+
   public void skipDays(int numberOfDays) {
     skippedDays += numberOfDays;
   }
