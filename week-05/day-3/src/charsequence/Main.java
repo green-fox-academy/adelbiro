9f9b52921cf02ea77036219ffc7fba97cc7acb85
Adél Bíró <b.adel93@gmail.com>
2018-11-21 15:53:13 +0100

    added shifter method

--- week-05/day-3/src/charsequence/Main.java
+++ week-05/day-3/src/charsequence/Main.java
@@ -7,8 +7,8 @@ public class Main {
     System.out.println(g.charAt(1));
 // should print out: l
 //
-//    Shifter s = new Shifter("example", 2);
-//    System.out.println(s.charAt(0));
+    Shifter s = new Shifter("example", 2);
+    System.out.println(s.charAt(0));
 //// should print out: a
   }
 }
--- week-05/day-3/src/charsequence/Shifter.java
+++ week-05/day-3/src/charsequence/Shifter.java
@@ -1,4 +1,26 @@
 package charsequence;
 
-public class Shifter {
+public class Shifter implements CharSequence {
+  String shifted;
+  int num;
+
+  public Shifter(String string, int num) {
+    shifted = string;
+    this.num = num;
+  }
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public char charAt(int index) {
+    return shifted.charAt(index + num);
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return null;
+  }
 }
