711b08cac3d1aeebc30a596cf60dd46741c44c67
Adél Bíró <b.adel93@gmail.com>
2018-12-12 12:12:06 +0100

    create: project structure

---
+++ week-08/day-03/bank-of-simba/src/main/java/greenfox/thymeleaf_practice/BankAccount.java
@@ -0,0 +1,27 @@
+package greenfox.thymeleaf_practice;
+
+public class BankAccount {
+
+  private String name;
+  private double balance;
+  private String animalType;
+
+  public BankAccount(String name, double balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+}
+
---
+++ week-08/day-03/bank-of-simba/src/main/java/greenfox/thymeleaf_practice/BankController.java
@@ -0,0 +1,30 @@
+package greenfox.thymeleaf_practice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class BankController {
+  BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+  List<BankAccount> accountList;
+
+  @RequestMapping (path="/show")
+  public String show(Model model) {
+    model.addAttribute("account", bankAccount);
+    return "index";
+  }
+
+
+
+  @ModelAttribute
+  public void addAttributes(Model model) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("animalType", bankAccount.getAnimalType());
+  }
+
+}
