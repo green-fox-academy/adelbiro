291c9912cc48b7baa7bd9acd51e9130f7b13b0c4
adelbiro <b.adel93@gmail.com>
2019-01-07 15:14:50 +0100

    add: appendA endpoint

--- week-10/day-01/restdemo/src/main/java/greenfox/rest/controller/RController.java
+++ week-10/day-01/restdemo/src/main/java/greenfox/rest/controller/RController.java
@@ -1,10 +1,12 @@
 package greenfox.rest.controller;
 
+import greenfox.rest.model.Appended;
 import greenfox.rest.model.Doubled;
 import greenfox.rest.model.ErrorMessage;
 import greenfox.rest.service.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public class RController {
   public Object greet(@RequestParam(name="name", required = false) String name,
                       @RequestParam( name="title", required = false) String title ) {
     return service.greeting(name, title);
+  }
+
+  @GetMapping("/appenda/{appendable}")
+  public Appended appendA(@PathVariable(name = "appendable") String appendable) {
+    return new Appended(appendable);
   }
 }
---
+++ week-10/day-01/restdemo/src/main/java/greenfox/rest/model/Appended.java
@@ -0,0 +1,18 @@
+package greenfox.rest.model;
+
+public class Appended {
+
+  private String appended;
+
+  public Appended(String appendable) {
+    this.appended = appendable + "a";
+  }
+
+  public String getAppended() {
+    return appended;
+  }
+
+  public void setAppended(String appended) {
+    this.appended = appended;
+  }
+}
