b4f2f9a97ea5e3721a322a54d648638975a740c5
adelbiro <b.adel93@gmail.com>
2019-01-15 12:14:58 +0100

    create: advanced spring project

---
+++ project-phase-0th-week/src/main/java/greenfox/springadvanced/SpringAdvancedApplication.java
@@ -0,0 +1,14 @@
+package greenfox.springadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAdvancedApplication.class, args);
+  }
+
+}
+
