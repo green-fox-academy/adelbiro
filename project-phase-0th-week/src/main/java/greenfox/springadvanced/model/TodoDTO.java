e7709b7a7b17cea42a9fd22362db95832fce7d1f
adelbiro <b.adel93@gmail.com>
2019-01-18 14:51:14 +0100

    add: modelmapper, basic entity to dto conversion

---
+++ project-phase-0th-week/src/main/java/greenfox/springadvanced/model/TodoDTO.java
@@ -0,0 +1,31 @@
+package greenfox.springadvanced.model;
+
+public class TodoDTO {
+
+  long id;
+  String title;
+
+  public TodoDTO() {
+  }
+
+  public TodoDTO(long id, String title) {
+    this.id = id;
+    this.title = title;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
--- project-phase-0th-week/src/main/java/greenfox/springadvanced/service/TodoServiceImpl.java
+++ project-phase-0th-week/src/main/java/greenfox/springadvanced/service/TodoServiceImpl.java
@@ -1,7 +1,9 @@
 package greenfox.springadvanced.service;
 
 import greenfox.springadvanced.model.Todo;
+import greenfox.springadvanced.model.TodoDTO;
 import greenfox.springadvanced.repository.TodoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,13 @@ import java.util.List;
 @Service
 public class TodoServiceImpl implements TodoService {
 
+  ModelMapper modelMapper = new ModelMapper();
 
   @Autowired
   TodoRepository repository;
 
+
+  //maybe I should take todoDTO-s?
   @Override
   public void addTodo(Todo todo) {
     if (todo!= null)
@@ -26,13 +31,22 @@ public class TodoServiceImpl implements TodoService {
     return new ArrayList(repository.findAll());
   }
 
+  @Override
   public List<Todo> findTodosContaining(String text) {
     return new ArrayList(repository.findTodosByTitleContaining(text));
-
   }
 
-  public Todo findTodo(long id) {
-    return repository.findById(id);
+  //is this it? should there be more?
+  public TodoDTO findTodo(long id) {
+    return convertEntityToDTO(repository.findById(id));
+  }
+
+  public TodoDTO convertEntityToDTO(Todo source) {
+    TodoDTO destination = new TodoDTO();
+    modelMapper.map(source, destination);
+    System.out.println(destination.getId());
+    System.out.println(destination.getTitle());
+    return destination;
   }
 
 }
